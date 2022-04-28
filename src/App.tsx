a7e35b52963720b0888d44bbdad78bdea1ab55b7
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-28 23:15:55 +0300

    add error message style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Button} from "./components/Button";
 import s from "./components/components.module.css"
@@ -8,21 +8,27 @@ import {SetBoard} from "./components/SetBoard";
 function App() {
     const [startScore, setStartScore] = useState<number>(0)
     const [maxScore, setMaxScore] = useState<number>(0)
-    const [score, setScore] = useState<number>(startScore)
+    const [value, setValue] = useState<number>(startScore)
     const [incDisabled, setIncDisabled] = useState<boolean>(false)
     const [setDisabled, setSetDisabled] = useState<boolean>(false)
+    useEffect(()=>{
+        localStorage.setItem("start", JSON.stringify(startScore))
+    }, [startScore])
+    useEffect(()=>{
+        localStorage.setItem("max", JSON.stringify(maxScore))
+    }, [maxScore])
     const onClickInc = () => {
-        const newScore = score + 1
-        setScore(newScore)
-        newScore === maxScore && setIncDisabled(true)
+        const newValue = value + 1
+        setValue(newValue)
+        newValue === maxScore && setIncDisabled(true)
     }
     const onClickReset = () => {
-        setScore(0)
+        setValue(startScore)
         setIncDisabled(false)
     }
     const onClickSetValue = () => {
-        localStorage.setItem("max", JSON.stringify(maxScore))
-        localStorage.setItem("start", JSON.stringify(startScore))
+   /*     localStorage.setItem("max", JSON.stringify(maxScore))
+        localStorage.setItem("start", JSON.stringify(startScore))*/
 
         console.log(localStorage.getItem("max"))
         console.log(localStorage.getItem("start"))
@@ -33,12 +39,15 @@ function App() {
     const onChangeMaxValue = (value: number) => {
         setMaxScore(value)
     }
+    const errorValue = maxScore===startScore || startScore<0
+
+
 
 
     return (
         <div className="App">
             <div className="block">
-                <ScoreBoard score={score}/>
+                <ScoreBoard value={value} startScore={startScore} maxScore={maxScore} errorValue={errorValue}/>
                 <span className={s.buttons}>
                     <Button callback={onClickInc} title={"INC"} disabled={incDisabled}/>
                     <Button callback={onClickReset} title={"RESET"}/>
--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import s from "./components.module.css"
 
 type ScoreBoardPropsType = {
-    score: number
+    value: number
+    startScore: number
+    maxScore: number
+    errorValue: boolean
 }
 export const ScoreBoard = (props: ScoreBoardPropsType) => {
-    const scoreClassName = props.score>=5 ? s.scoreMore : ""
+    const scoreClassName = props.errorValue ? s.errorMessageBoard : (props.value===props.maxScore) ? s.scoreMore  : ""
+    const errorMessage = "enter values and press 'set'"
     return (
         <div className={s.scoreBoard}>
-            <div className={scoreClassName}>{props.score}</div>
+            <div className={scoreClassName}>{props.errorValue ? errorMessage : props.value}</div>
         </div>
     );
 }
